19cc1ec7f24e7622923b00332ce755458d461c16
PatoJZ <patojimenez08@gmail.com>
2023-03-23 19:45:00 -0300

    ejercicio 4

--- labSemana2_ejercicio2.c
+++ labSemana2_ejercicio2.c
@@ -1,51 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define N 100
-void fusionararrays(int a[], int b[],int cant_a, int cant_b){
-    int limit = 0;
+
+void fusionararrays(int a[], int b[], int itemsLimites[], int cant_a, int cant_b){
     int c[N] = {0};
-    for(int i=0;i<cant_a;i++){
-        a[i]=c[i];
-        limit++;
+    int cant_mayor = 0;
+    int i;
+
+    if (cant_a > cant_b){
+        cant_mayor = cant_a;
     }
-    for(int i=limit;i<cant_b + limit;i++){
-        b[i]=c[i];
-        limit++;
-        if (limit > N){
-            break;
+    if (cant_b > cant_a){
+        cant_mayor = cant_b;
+    }
+    for(i=0;i<cant_mayor;i++){
+        if (itemsLimites[i]<= a[i] + b[i]){
+            c[i] = itemsLimites[i];
+            printf("\nlimite excedido en el item %d que son %d\n", i, itemsLimites[i]);
+        }else{
+             c[i] = a[i]+ b[i];
         }
     }
-    for(int i=0;i<limit;i++){
-        printf("array nuevo\n");
-        printf("el indice %d contiene el elemento %d\n",i,c[i]);
+    printf("\n--Inventario nuevo--\n");
+    for(i=0;i<cant_mayor;i++){
+        printf("el Item %d contiene %d elementos\n",i,c[i]);
     }
 }
 
 int main(){
-    int arr1[N] ={0};
-    int arr2[N] ={0};
+    srand(time(NULL));
+
+    int arr1[N] = {0};
+    int arr2[N] = {0};
+    int arr3[N] = {0};
     int cant1=0;
     int cant2=0;
-    int freq=0;
     int i;
+    for (i=0;i<N; i++){
+        arr3[i] = rand()%20+1;
+    }
     while (cant1 <= 0){
-        printf("cuantos items tienes jugador 1:");
+        printf("\ncuantos items tienes jugador 1:");
         scanf("%d",&cant1);
     }
     for (i = 0; i < cant1; i++){
-        printf("cuantos item hay del item %d\n ", i);
+        printf("cuantos item hay del item [%d] ", i);
         scanf("%d", &arr1[i]);
     }
     while (cant2 <= 0){
-        printf("cuantos items tienes jugador 2:");
+        printf("\ncuantos items tienes jugador 2: ");
         scanf("%d",&cant2);
     }
     for (i = 0; i < cant2; i++){
-        printf("cuantos item hay del item %d\n ", i);
+        printf("cuantos item hay del item [%d]: ", i);
         scanf("%d", &arr2[i]);
     }
-    fusionararrays(arr1, arr2, cant1, cant2);
-    return 0;
-
-    
+    fusionararrays(arr1, arr2, arr3, cant1, cant2);
+    return 0; 
     
 }
--- labSemana2_ejercicio4.c
+++ labSemana2_ejercicio4.c
@@ -5,48 +5,67 @@
 #define DIAS_SEMANA 7
 #define TEMPERATURA_FIEBRE 37.5
 
+float Conversion_Fahrenheit(float celsius){
+    return (float)((celsius * 9.0 / 5.0) + 32.0);
+}
+
 int main()
 {
     float temperaturas[MAX_PACIENTES][DIAS_SEMANA];
-    char nombres[MAX_PACIENTES][50];
-    char habitaciones[MAX_PACIENTES][10];
+    char nombres[MAX_PACIENTES][50] = {{"N/A"}};
+    char habitaciones[MAX_PACIENTES][10] = {{"N/A"}};
     int edades[MAX_PACIENTES];
     int n;
-
-    printf("Ingrese la cantidad de pacientes (maximo %d): ", MAX_PACIENTES);
+    int mostrarEnFahrenheit;
+    int opcionIngreso;
+    int i;
+    int j;
+    printf("Ingrese la cantidad de pacientes (maximo %d): \n", MAX_PACIENTES);
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        printf("Ingrese el nombre del paciente %d: ", i+1);
-        scanf("%s", nombres[i]);
+    for ( i = 0; i < n; i++){
+        printf("Pulse [1] para ingresar Nombre\n");
+        printf("Pulse [2] para ingresar habitacion y edad\n");
+        scanf("%d",&opcionIngreso);
 
-        printf("Ingrese el numero de habitacion del paciente %d: ", i+1);
-        scanf("%s", habitaciones[i]);
-
-        printf("Ingrese la edad del paciente %d: ", i+1);
-        scanf("%d", &edades[i]);
-
+        if (opcionIngreso == 1){
+            printf("Ingrese el nombre del paciente %d: ", i+1);
+            scanf("%s", nombres[i]);
+        }else if(opcionIngreso == 2){
+            printf("Ingrese el numero de habitacion del paciente %d: ", i+1);
+            scanf("%s", habitaciones[i]);
+            printf("Ingrese la edad del paciente %d: ", i+1);
+            scanf("%d", &edades[i]);
+        }
         printf("Ingrese las temperaturas del paciente %d para los ultimos %d dias:\n", i+1, DIAS_SEMANA);
-        for (int j = 0; j < DIAS_SEMANA; j++) {
+        for ( j = 0; j < DIAS_SEMANA; j++){
             printf("Dia %d: ", j+1);
             scanf("%f", &temperaturas[i][j]);
         }
     }
 
+    printf("Desea ver las temperaturas en Fahrenheit? (1 = Si, 0 = No): ");
+    scanf("%d", &mostrarEnFahrenheit);
+
     printf("\nTemperaturas promedio de los pacientes:\n");
-    for (int i = 0; i < n; i++) {
+    for ( i = 0; i < n; i++){
         float sum = 0;
-        for (int j = 0; j < DIAS_SEMANA; j++) {
+        for ( j = 0; j < DIAS_SEMANA; j++){
             sum += temperaturas[i][j];
         }
         float promedio = sum / DIAS_SEMANA;
-        printf("%s - Habitacion %s: %.2f°C\n", nombres[i], habitaciones[i], promedio);
+        if (mostrarEnFahrenheit == 1){
+            promedio = Conversion_Fahrenheit(promedio);
+            printf("%s - Habitacion %s: %.2f grados Fahrenheit\n", nombres[i], habitaciones[i], promedio);
+        } else {
+            printf("%s - Habitacion %s: %.2f grados Celsius\n", nombres[i], habitaciones[i], promedio);
+        }
     }
 
     printf("\nPacientes con fiebre durante tres o mas dias consecutivos:\n");
-    for (int i = 0; i < n; i++) {
+    for ( i = 0; i < n; i++) {
         int consecutivos = 0;
-        for (int j = 0; j < DIAS_SEMANA; j++) {
+        for ( j = 0; j < DIAS_SEMANA; j++) {
             if (temperaturas[i][j] >= TEMPERATURA_FIEBRE) {
                 consecutivos++;
                 if (consecutivos >= 3) {
